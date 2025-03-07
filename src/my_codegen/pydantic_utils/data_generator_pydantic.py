3603520a535a697221d1a5ec67a82c102b30b145
evgeniy_vladimirov <vladimiroves.qa@gmail.com>
2025-03-07 20:09:55 +0700

    added cde import for facade template

--- src/my_codegen/pydantic_utils/data_generator_pydantic.py
+++ src/my_codegen/pydantic_utils/data_generator_pydantic.py
@@ -13,8 +13,6 @@ from my_codegen.pydantic_utils.pydantic_config import BaseConfigModel
 
 from pydantic import Field, StringConstraints, RootModel
 
-from src.my_codegen.http_clients.cde.models import CreateApprovalProcessTemplateRequest
-
 fake = Faker()
 
 
